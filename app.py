77c5fc47739f5f530e026ef44eb7b998d8f69bd6
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-01-11 15:39:53 +0530

    added db and db model

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask , render_template
 
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app=Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expense.db"
+
+db = SQLAlchemy(app)
+
+class Expense (db.Model):
+  id=  db.Column(db.Integer,primary_key=True)
+  description=  db.Column(db.String(200),nullable=False)
+  amount=  db.Column(db.Float,nullable=False)
+  category=  db.Column(db.Integer,Primary_key=True)
+  date=  db.Column(db.Date,nullable=False,default=date.today())
+
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
